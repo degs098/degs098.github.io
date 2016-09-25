797e2fb2871c4334091fe3c81e02376e4476e4bd
Daniel Gallego <gallegodaniel9004@gmail.com>
2016-09-25 14:40:36 -0500

    Se agrega el series en la grafica y se hacen cambios en el color de las barras y el input

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -6,15 +6,16 @@
  * Controller of the tallerDadoApp
  */
 
-var arregloData = [0,0,0,0,0,0];
+
 
 angular.module('tallerDadoApp',["chart.js"]).controller('MainCtrl', ['$scope', 'calcularFrecuenciaService', function ($scope, calcularFrecuenciaServicio) {    
     $scope.labels = ['Cara 1', 'Cara 2', 'Cara 3', 'Cara 4', 'Cara 5', 'Cara 6'];      
     $scope.series = ['Frecuencia'];
+    $scope.colors = ['#1e88e5'];
     $scope.experimentoCant = null;      
     $scope.frecuenciaCaras = function(){    
-      arregloData = calcularFrecuenciaServicio.calcularFrecuencia($scope.experimentoCant);  
-      $scope.data = arregloData;
+      $scope.arregloData = calcularFrecuenciaServicio.calcularFrecuencia($scope.experimentoCant);  
+      $scope.data = [$scope.arregloData];
     };  
 }]);
 
